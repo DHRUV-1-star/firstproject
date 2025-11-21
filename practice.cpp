ca508d13c0641ae3ab34ed9bf11acdb18a6e8f6e
DHRUV-1-star <ravaldhruvp1112@gmail.com>
2025-11-21 18:47:37 +0530

    add c++ file

--- practice.cpp
+++ practice.cpp
@@ -5,13 +5,13 @@ int main(){
     char ch='A';
     for(i=0;i<n;i++)
     {
-        for(j=0;j<n;j++)
+        for(j=0;j<i+1;j++)
         {
             cout<<ch;
-            ch++;
+            
         }
+        ch++;
         cout<<endl;
-        
     }
     return 0;
 }
