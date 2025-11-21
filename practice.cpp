84b89c83838923be251250ff7160f90d7cd35d7a
DHRUV-1-star <ravaldhruvp1112@gmail.com>
2025-11-21 17:39:18 +0530

    add c++ file

---
+++ practice.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j,n=5;
+    char ch='A';
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            cout<<ch;
+            ch++;
+        }
+        cout<<endl;
+        
+    }
+    return 0;
+}
